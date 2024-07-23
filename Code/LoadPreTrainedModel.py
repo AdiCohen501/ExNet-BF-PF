f348033741ff21b703a4056c6a25c48bd17f9018
AdiCohen501 <103136942+AdiCohen501@users.noreply.github.com>
2024-07-23 22:15:52 +0300

    Update LoadPreTrainedModel.py

--- Code/LoadPreTrainedModel.py
+++ Code/LoadPreTrainedModel.py
@@ -7,7 +7,7 @@ def loadPreTrainedModel(cfg):
     model = ExNetBFPF(cfg.modelParams)
 
     if cfg.paths.rev_env:  # Reverberant Environment
-        PATH = cfg.paths.modelData_path + 'trained_model_dataset_withRev_two_step_approach.pt'
+        PATH = cfg.paths.modelData_path + 'trained_model_dataset_withRev.pt'
         # Load the model with the best saved weights
         model = torch.load(PATH)
 
